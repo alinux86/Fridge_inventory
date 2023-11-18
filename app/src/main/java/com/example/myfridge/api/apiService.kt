f4ef3d6b004480aa053ae1115cdc1d4d837717cc
Alexander Mussitelli <alexander.mussitelli@gmail.com>
2023-11-18 17:06:42 +0100

    working on api

--- app/src/main/java/com/example/myfridge/api/apiService.kt
+++ app/src/main/java/com/example/myfridge/api/apiService.kt
@@ -1,9 +1,11 @@
 package com.example.myfridge.api
 
+import com.example.myfridge.model.ProductInfo
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
 
-class apiService {
+interface apiService {
 
+    @GET("/api/v2/product/{barcode}")
+    fun getProductInfo() : Call<List<ProductInfo>>
 }
---
+++ app/src/main/java/com/example/myfridge/model/ProductInfo.kt
@@ -0,0 +1,8 @@
+package com.example.myfridge.model
+// String? allow nullable, in case there is no information about a val
+data class ProductInfo(
+    val productName : String?,
+    val brand : String?,
+    val ecoscore : String?
+
+)
